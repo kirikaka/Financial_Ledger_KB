f36935429abfb48506798869f47db6ccd58c6bc7
jeonkyujin <954qew@naver.com>
2025-04-07 16:55:39 +0900

    FEAT: 로그인 및 회원가입 페이지 구현

--- Financial_Ledger/src/router/index.js
+++ Financial_Ledger/src/router/index.js
@@ -1,14 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Login from '@/components/Login.vue'; // Login.vue 경로 맞춰서 수정
+import SignUp from '@/components/SignUp.vue';
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+  },
+  { path: '/signup', name: 'SignUp', component: SignUp },
+  // 나중에 다른 페이지도 추가 가능
+  // {
+  //   path: '/dashboard',
+  //   name: 'Dashboard',
+  //   component: () => import('@/pages/Dashboard.vue')
+  // }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
