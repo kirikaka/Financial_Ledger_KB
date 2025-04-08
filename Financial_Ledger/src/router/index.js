66e84951d29a8f07bd5e0297d2a416b280b2eda2
hojin <issac1118@kakao.com>
2025-04-08 13:08:38 +0900

    CHORE: axios 관련 설정 추가 및 index.js 추가

--- Financial_Ledger/src/router/index.js
+++ Financial_Ledger/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/components/Login.vue'; // Login.vue 경로 맞춰서 수정
 import SignUp from '@/components/SignUp.vue';
+import HomePage from '@/components/HomePage.vue';
 const routes = [
   {
     path: '/',
@@ -8,6 +9,7 @@ const routes = [
     component: Login,
   },
   { path: '/signup', name: 'SignUp', component: SignUp },
+  { path: '/homepage', name: 'HomePage', component: HomePage },
   // 나중에 다른 페이지도 추가 가능
   // {
   //   path: '/dashboard',
